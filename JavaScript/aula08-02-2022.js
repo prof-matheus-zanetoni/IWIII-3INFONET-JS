b924ea884549248c04172afec39a2fef230edab2
Matheus Galbero Zanetoni <matheus.zanetoni@etec.sp.gov.br>
2022-02-21 23:47:17 -0300

    Aula do dia 08/02/2022

---
+++ JavaScript/aula08-02-2022.js
@@ -0,0 +1,5 @@
+//window.alert("Olá, mundo!");
+//window.confirm("Olá, mundo!");
+var nome = window.prompt("Digite seu nome: ");
+//window.alert("Olá, " + nome); --> Concatenação
+window.alert(`Olá, ${nome}`); //--> Interpolação
