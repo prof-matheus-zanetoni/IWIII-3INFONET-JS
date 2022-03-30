4eec192f1a40c4d67ff495c42da76c5545f7cf18
Matheus Galbero Zanetoni <matheus.zanetoni@etec.sp.gov.br>
2022-03-30 12:15:14 -0300

    Laços de repetição e vetores

---
+++ JavaScript/aula29-03.2022.js
@@ -0,0 +1,47 @@
+//Laços de repetição e vetores
+
+/*let c = 1;
+
+while(c <= 10) {
+    console.log(c);
+    c++;
+}*/
+
+/*let b = 10;
+
+while(b >= 0) {
+    console.log(b);
+    b--;
+}*/
+
+/*let a = 0;
+
+do {
+    console.log(a);
+    a++;
+} while(a < 50);*/
+
+/*for(let i = 0; i <= 100; i+=10) {
+    console.log(i);
+}*/
+
+/*for(let i = 100; i >= 0 ; i-=10) {
+    console.log(i);
+}*/
+
+//let vetor = ['A', "Matheus", 22, 45, 'N', 12.79];
+
+//vetor.push("Teste");
+//vetor.pop();
+//vetor.shift();
+//vetor.unshift("Terminando JS");
+//console.log(vetor.slice(2, 6));
+//console.log(vetor.indexOf("Matheus"));
+/*for(let p = 0; p < vetor.length; p++) {
+    console.log(vetor[p]);
+}*/
+
+//let nome = "José Paulo Codinhoto";
+//console.log(nome.slice(11, 20));
+//let nomes = nome.split(" ");
+//console.log(nomes[nomes.length - 1]);
